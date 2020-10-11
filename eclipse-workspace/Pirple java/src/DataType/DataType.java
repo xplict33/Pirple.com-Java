98f9b16c0bef5055bb9701921c0af79fe43284b9
xplict33 <obiakarije@gmail.com>
2020-10-11 10:39:06 -0400

    What's your favorite song?
    Think of all the attributes that you could use to describe that song.
    That is: all of it's details or "meta-data". These are attributes like
    "Artist", "Year Released", "Genre", "Duration", etc. Think of as many
    different characteristics as you can.
    
    Create a new project and within your main file list all of the
    attributes of the song, one after another. Create a variable for each
    attribute, and give each variable a value. Here's an example:
    
    ```
    String genre = "Jazz";
    
    ```
    
    Write each variable on its own line. Then, after you have listed the
    variables, print each one of them out. Think of as many characteristics
    of the song as you can. Try to use all of the data types you have
    learned so far.

---
+++ eclipse-workspace/Pirple java/src/DataType/DataType.java
@@ -0,0 +1,25 @@
+package DataType;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		String Artist = "Remi Wolf";
+        String Song = "Monte Carlo";
+        String Genre = "Pop";
+        float DurationInSeconds = (float) 2.24;
+        int year = 2020;
+        String RecordLabel = "Island Records";
+        String Director = "Austin Call";
+        char me = 'H';
+        
+        System.out.println(Artist);
+        System.out.println(Song);
+        System.out.println(Genre);
+        System.out.println(DurationInSeconds);
+        System.out.println(year);
+        System.out.println(RecordLabel);
+        System.out.println(Director);
+        System.out.println(me);
+	}
+
+}
